6e469c8c642296a6f932ca2415fc1f01a5d1457d
ohansyah <ohansyah@weekendinc.com>
2024-09-01 19:19:48 +0700

    feat: implement themes sneat

---
+++ app/Views/layouts/main.php
@@ -0,0 +1,32 @@
+<?= $this->include('partials/header') ?>
+
+<!-- Layout wrapper -->
+<div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+    
+    <!-- Menu -->
+    <?= $this->include('partials/menu') ?>
+    <!-- / Menu -->
+
+    <!-- Layout container -->
+    <div class="layout-page">
+
+        <!-- Content wrapper -->
+        <div class="content-wrapper">
+        
+            <!-- Content -->
+                <?= $this->renderSection('content') ?>
+            <!-- / Content -->
+        
+            <div class="content-backdrop fade"></div>
+        </div>
+        <!-- Content wrapper -->
+    </div>
+    <!-- / Layout page -->
+
+    <!-- Overlay -->
+    <div class="layout-overlay layout-menu-toggle"></div>
+</div>
+<!-- / Layout wrapper -->
+
+<?= $this->include('partials/footer') ?>
---
+++ app/Views/pages/home.php
@@ -0,0 +1,8 @@
+<!-- home page -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Home Page</h1>
+        </div>
+    </div>
+</div>
